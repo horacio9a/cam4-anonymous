636dd6da97481c02bdd01b5d677d7619b09371a2
horacio9a <horacio9a@gmail.com>
2021-05-23 01:33:41 +0200

    v.2.0.0 Cam4 Anonymous Get Online All Models for Python27

---
+++ C4_getOnlineAllModels.py
@@ -0,0 +1,33 @@
+# Cam4 Anonymous Get Online All Models v.2.0.0 by horacio9a for Python 2.7.18
+# coding: utf-8
+
+import sys, os, urllib, ssl, re, json, requests
+from sys import exit
+offline = False
+
+def getOnlineUsers(page):
+    attempt = 1
+    while attempt < 3:
+        try:
+            results = requests.get("https://www.cam4.com/directoryCams?directoryJson=true&online=true&url=true&page={}".format(page)).json()
+            return results
+        except:
+            attempt = attempt + 1
+
+if __name__ == '__main__':
+    while True:
+        attempt = 1
+        online = []
+        while not offline:
+            results = getOnlineUsers(attempt)
+            if len(results['users']) > 1:
+                online.extend([user['username'] for user in results['users']])
+            else:
+                offline = True
+            attempt = attempt + 1
+        offline = False
+        for username in online:
+            online_all_model_list = username.lower()
+            print(online_all_model_list)
+        if attempt > 3:
+            exit(0)
