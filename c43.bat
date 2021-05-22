7dd676b90bf028eefc116083e2e136f1c3980f1e
horacio9a <horacio9a@gmail.com>
2021-05-22 16:43:44 +0200

    v.2.0.0 Batch script for use all scripts for Python39

--- c43.bat
+++ c43.bat
@@ -3,23 +3,48 @@ SETLOCAL EnableDelayedExpansion
 :START
 CLS
 ECHO.
-CLS && ECHO #######################################################################
-ECHO ###   C A M 4   A N O N Y M O U S   P Y T H O N   3   S C R I P T   ###
-ECHO #######################################################################
+CLS && ECHO #############################################################################
+ECHO ###    C A M 4    A N O N Y M O U S    P Y T H O N    3    S C R I P T    ###
+ECHO #############################################################################
 ECHO.
-SET /P MODE=EXIT(5)  C4YTR3(4)  C4SLR3(3)  C4FFR3(2)  C4RR3(1)  C43(0)(ENTER)(%MODE%): 
-IF "%MODE%"=="" GOTO C43
-IF "%MODE%"=="0" GOTO C43
-IF "%MODE%"=="1" GOTO C4RR3
-IF "%MODE%"=="2" GOTO C4FFR3
-IF "%MODE%"=="3" GOTO C4SLR3
-IF "%MODE%"=="4" GOTO C4YTR3
-IF "%MODE%"=="5" GOTO EXIT
+SET /P MODE=EXIT(6) C4YTR3(5) C4SLR3(4) C4FFR3(3) C43(2) GETOW3(1) GETOA3(0)(ENTER)(%MODE%): 
+IF "%MODE%"=="" GOTO GETOA3
+IF "%MODE%"=="0" GOTO GETOA3
+IF "%MODE%"=="1" GOTO GETOW3
+IF "%MODE%"=="2" GOTO C43
+IF "%MODE%"=="3" GOTO C4FFR3
+IF "%MODE%"=="4" GOTO C4SLR3
+IF "%MODE%"=="5" GOTO C4YTR3
+IF "%MODE%"=="6" GOTO EXIT
+:GETOA3
+ECHO.
+CLS && ECHO ###################################################
+ECHO ### GETOA3 ###  O N L I N E   A L L   L I S T  ####
+ECHO ###################################################
+cd C:/
+COLOR 0F
+cd -c4-py
+python C4_getOnlineAllModels3.py > C4_Online_All.txt
+ECHO.
+PAUSE
+GOTO START
+:GETOW3
+ECHO.
+CLS && ECHO #########################################################
+ECHO ### GETOW3 ###  O N L I N E   W A N T E D   L I S T  ####
+ECHO #########################################################
+cd C:/
+COLOR 0F
+cd -c4-py
+python C4_getOnlineWantedModels3.py > C4_Online_Wanted.txt
+ECHO.
+PAUSE
+GOTO START
 :C43
 ECHO.
-CLS && ECHO #################################################
-ECHO ### C4 #####  P Y T H O N   R E C / P L A Y  ####
-ECHO #################################################
+CLS && ECHO ################################################
+ECHO ### C43 ###### R E C O R D I N G ###############
+ECHO ################################################
 cd C:/
 COLOR 0F
 cd -c4-py
@@ -27,65 +52,31 @@ python c43.py
 ECHO.
 PAUSE
 GOTO START
-:C4RR3
-ECHO.
-SET n=0
-FOR /F "tokens=*" %%A IN (C:/-c4-py/C4_Model.txt) DO (
-SET /A n=n+1
-SET _fav!n!=%%A
-ECHO !n! %%A
-)
-ECHO.
-SET /P MODEL=Choose C4 Model Name (%M% %MODEL%): 
-FOR /L %%f IN (1,1,!n!) DO (
-IF /I '%MODEL%'=='%%f' SET M=%%f
-)
-SET n=0
-FOR /F "tokens=*" %%A IN (C:/-c4-py/C4_Model.txt) DO (
-SET /A n=n+1
-IF !n!==%M% SET MODEL=%%A
-)
-:C4RR3_
-ECHO.
-SET MODELNAME=%MODEL% ############ %M% ############################
-SET _MODEL_=%MODELNAME:~0,34%
-ECHO.
-CLS && ECHO ##################################################
-ECHO ### C4RR3 ###### P Y T H O N   R E C #### 24/7 ###
-ECHO ### RTMP ###### %_MODEL_%
-ECHO ##################################################
-cd C:/
-COLOR 0F
-cd -c4-py
-python c4rr3.py %MODEL%
-TIMEOUT 30
-GOTO C4RR3_
 :C4FFR3
-ECHO.
 SET n=0
-FOR /F "tokens=*" %%A IN (C:/-c4-py/C4_Model.txt) DO (
+FOR /F "tokens=*" %%A IN (C:/-c4-py/C4_Wanted.txt) DO (
 SET /A n=n+1
 SET _fav!n!=%%A
 ECHO !n! %%A
 )
 ECHO.
-SET /P MODEL=Choose C4 Model Name (%M% %MODEL%): 
+SET /P MODEL=Choose C4 Model Name (%M%): 
 FOR /L %%f IN (1,1,!n!) DO (
 IF /I '%MODEL%'=='%%f' SET M=%%f
 )
 SET n=0
-FOR /F "tokens=*" %%A IN (C:/-c4-py/C4_Model.txt) DO (
+FOR /F "tokens=*" %%A IN (C:/-c4-py/C4_Wanted.txt) DO (
 SET /A n=n+1
 IF !n!==%M% SET MODEL=%%A
 )
+ECHO.
+SET MODELNAME=%MODEL% #####################################
+SET _MODEL_=%MODELNAME:~0,34%
 :C4FFR3_
 ECHO.
-SET MODELNAME=%MODEL% ############ %M% ############################
-SET _MODEL_=%MODELNAME:~0,34%
-ECHO.
 CLS && ECHO ##################################################
-ECHO ### C4FFR3 ##### P Y T H O N   R E C #### 24/7 ###
-ECHO ### FFMPEG #### %_MODEL_%
+ECHO ### C4FFR3 #### R E C O R D I N G ################
+ECHO ############### %_MODEL_%
 ECHO ##################################################
 cd C:/
 COLOR 0F
@@ -94,31 +85,30 @@ python c4ffr3.py %MODEL%
 TIMEOUT 30
 GOTO C4FFR3_
 :C4SLR3
-ECHO.
 SET n=0
-FOR /F "tokens=*" %%A IN (C:/-c4-py/C4_Model.txt) DO (
+FOR /F "tokens=*" %%A IN (C:/-c4-py/C4_Wanted.txt) DO (
 SET /A n=n+1
 SET _fav!n!=%%A
 ECHO !n! %%A
 )
 ECHO.
-SET /P MODEL=Choose C4 Model Name (%M% %MODEL%): 
+SET /P MODEL=Choose C4 Model Name (%M%): 
 FOR /L %%f IN (1,1,!n!) DO (
 IF /I '%MODEL%'=='%%f' SET M=%%f
 )
 SET n=0
-FOR /F "tokens=*" %%A IN (C:/-c4-py/C4_Model.txt) DO (
+FOR /F "tokens=*" %%A IN (C:/-c4-py/C4_Wanted.txt) DO (
 SET /A n=n+1
 IF !n!==%M% SET MODEL=%%A
 )
+ECHO.
+SET MODELNAME=%MODEL% #####################################
+SET _MODEL_=%MODELNAME:~0,34%
 :C4SLR3_
 ECHO.
-SET MODELNAME=%MODEL% ############ %M% ############################
-SET _MODEL_=%MODELNAME:~0,34%
-ECHO.
 CLS && ECHO ##################################################
-ECHO ### C4SLR3 ##### P Y T H O N   R E C #### 24/7 ###
-ECHO ### SL ######## %_MODEL_%
+ECHO ### C4SLR3 #### R E C O R D I N G ################
+ECHO ############### %_MODEL_%
 ECHO ##################################################
 cd C:/
 COLOR 0F
@@ -127,31 +117,30 @@ python c4slr3.py %MODEL%
 TIMEOUT 30
 GOTO C4SLR3_
 :C4YTR3
-ECHO.
 SET n=0
-FOR /F "tokens=*" %%A IN (C:/-c4-py/C4_Model.txt) DO (
+FOR /F "tokens=*" %%A IN (C:/-c4-py/C4_Wanted.txt) DO (
 SET /A n=n+1
 SET _fav!n!=%%A
 ECHO !n! %%A
 )
 ECHO.
-SET /P MODEL=Choose C4 Model Name (%M% %MODEL%): 
+SET /P MODEL=Choose C4 Model Name (%M%): 
 FOR /L %%f IN (1,1,!n!) DO (
 IF /I '%MODEL%'=='%%f' SET M=%%f
 )
 SET n=0
-FOR /F "tokens=*" %%A IN (C:/-c4-py/C4_Model.txt) DO (
+FOR /F "tokens=*" %%A IN (C:/-c4-py/C4_Wanted.txt) DO (
 SET /A n=n+1
 IF !n!==%M% SET MODEL=%%A
 )
+ECHO.
+SET MODELNAME=%MODEL% #####################################
+SET _MODEL_=%MODELNAME:~0,34%
 :C4YTR3_
 ECHO.
-SET MODELNAME=%MODEL% ############ %M% ############################
-SET _MODEL_=%MODELNAME:~0,34%
-ECHO.
 CLS && ECHO ##################################################
-ECHO ### C4YTR3 ##### P Y T H O N   R E C #### 24/7 ###
-ECHO ### YTDL ###### %_MODEL_%
+ECHO ### C4YTR3 #### R E C O R D I N G ################
+ECHO ############### %_MODEL_%
 ECHO ##################################################
 cd C:/
 COLOR 0F
