05fccb7da9c0a8af47ad8b2b3810b60e8625a5df
horacio9a <horacio9a@gmail.com>
2021-05-25 23:39:23 +0200

    v.2.0.1 Cam4 Remote 24/7 Anonymous STREAMLINK Recorder for Python27

--- c4slr.py
+++ c4slr.py
@@ -1,4 +1,4 @@
-# Cam4 Remote Anonymous STREAMLINK Recorder v.2.0.0 by horacio9a for Python 2.7.18
+# Cam4 Remote Anonymous STREAMLINK Recorder v.2.0.1 by horacio9a for Python 2.7.18
 # coding: utf-8
 
 import sys, os, urllib, urllib3, ssl, re, time, datetime, requests, random, command
